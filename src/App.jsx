8d75f11d75c4b44020b8b90a1c772e6aff09ab9c
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2022-07-26 18:17:48 -0500

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 export default function App() {
     return (
         <div>
             <h1>APP</h1>
+            <Outlet />
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import ReactDOM from "react-dom/client";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 import App from "./App";
 import './index.css'
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root")
 );
 root.render(
-    <App/>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route path="" element={<Home />} />
+                <Route path="about" element={<About />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
 );

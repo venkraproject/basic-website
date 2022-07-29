cb6d9e7a4b55623ed688314adbb21c60148bfd8f
Juan R. Cifuentes <juanrcifuentes@icloud.com>
2022-07-29 13:38:33 -0500

    updated singleproduct page styles

--- src/pages/singleProduct/SingleProduct.jsx
+++ src/pages/singleProduct/SingleProduct.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import Select from "react-select";
+// import Select from "react-select";
 import products from "../../data/products";
 import "./singleProduct.css";
 
@@ -7,12 +7,12 @@ const SingleProduct = () => {
     const { productId } = useParams();
     const product = products.find((product) => product.id === productId);
 
-    const sizes = [
-        { label: "XL - Disponibles: " + product.xl, value: "xl" },
-        { label: "L - Disponibles: " + product.l, value: "l" },
-        { label: "M - Disponibles: " + product.m, value: "m" },
-        { label: "S - Disponibles: " + product.s, value: "s" },
-    ];
+    // const sizes = [
+    //     { label: "XL - Disponibles: " + product.xl, value: "xl" },
+    //     { label: "L - Disponibles: " + product.l, value: "l" },
+    //     { label: "M - Disponibles: " + product.m, value: "m" },
+    //     { label: "S - Disponibles: " + product.s, value: "s" },
+    // ];
 
     const buyLink =
         "https://wa.me/+573044644001?text=Hola%2C%20estoy%20interesado%20en%20" +
@@ -31,24 +31,52 @@ const SingleProduct = () => {
             </div>
             <div className="container product-props__container">
                 <p>{product.description}</p>
-                <h5>Tamaño</h5>
-                <div className="select__container">
-                    <Select isSearchable={false} options={sizes} />
-                </div>
-                <h5>Colores:</h5>
-                <p>{product.colores_temp}</p>
-                <div className="container price-buy">
-                    <div>
-                        <h3>${product.price.toLocaleString()}</h3>
+                <div className="productSection">
+                    <h5 className="productSection__title">Tamaños</h5>
+                    <div className="select__container">
+                        <div className="selector__size-options">
+                            <div className="selector__size-option">
+                                <small>XL</small>
+                                <br/>
+                                {product.xl}
+                            </div>
+                            <div className="selector__size-option">
+                                <small>L</small>
+                                <br/>
+                                {product.l}
+                            </div>
+                            <div className="selector__size-option">
+                                <small>M</small>
+                                <br/>
+                                {product.m}
+                            </div>
+                            <div className="selector__size-option">
+                                <small>S</small>
+                                <br/>
+                                {product.s}
+                            </div>
+                        </div>
+                        {/* <Select isSearchable={false} options={sizes} /> */}
                     </div>
-                    <a
-                        href={buyLink}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="product__buy-link"
-                    >
-                        Compra ahora
-                    </a>
+                </div>
+                <div className="productSection">
+                    <h5>Colores:</h5>
+                    <p>{product.colores_temp}</p>
+                </div>
+                <div className="productSection">
+                    <div className="container price-buy">
+                        <div>
+                            <h3>${product.price.toLocaleString()}</h3>
+                        </div>
+                        <a
+                            href={buyLink}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="product__buy-link"
+                        >
+                            Compra ahora
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>

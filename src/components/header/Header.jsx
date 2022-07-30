bb810a850441bdeb4b7cd94c64a21336d175334c
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2022-07-30 14:33:00 -0500

    header update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import "./header.css";
 import {BsChevronDown} from "react-icons/bs"
 import img from "../../assets/full-logo.png"
+import VID from "../../assets/main.mp4"
 
 const Header = () => {
     let img_url = "https://i.postimg.cc/wjN7B58s/car-1.jpg";
     return (
         <section id="header">
-            <div className="header-background_img" style={{ backgroundImage:`url(${img_url})`,backgroundRepeat:"no-repeat",backgroundSize:"cover"   }}>
-                <div className="header-background">
-                    <div className="container header-title">
-                        <img src={img} alt=""/> 
-                    </div>
+            <video src={VID} autoPlay loop muted/>
+            <div className="header-content">
+                <div className="container-header_logo">
+                    <img src={img} alt=""/> 
                 </div>
                 <div className="header-scroll_down">
-                    SCROLL DOWN <br/> <BsChevronDown/>
+                    <div className="header-scroll_down-text">SCROLL DOWN</div> <br/> <div className="header-scroll_down-arrow"><BsChevronDown/></div>
                 </div>
             </div>
         </section>

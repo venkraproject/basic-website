1f1576c14cc138054b6336ccd1db3a23a5417442
Juan R. Cifuentes <42916798+JuanRCifuentes@users.noreply.github.com>
2026-01-31 12:12:47 +0100

    Update WhatsApp phone number in socialLinks.js

--- src/config/socialLinks.js
+++ src/config/socialLinks.js
@@ -7,7 +7,7 @@ const socialLinks = {
   instagram: "https://www.instagram.com/venkra.co/",
   messenger: "https://m.me/Venkra.co",
   whatsapp: "https://wa.me/message/PLACEHOLDER",
-  whatsappPhone: "+573044644001",
+  whatsappPhone: "+573044644055",
 };
 
 export default socialLinks;

e9da9d0ea9a6dde2bb1c3026d11c6c63d451ad06
venkraproject <106117632+venkraproject@users.noreply.github.com>
2026-01-31 08:59:59 -0500

    Update socialLinks.js

--- src/config/socialLinks.js
+++ src/config/socialLinks.js
@@ -20,7 +20,7 @@ const socialLinks = {
   whatsapp: {
     url: "https://wa.me/message/PLACEHOLDER",
     contactUrl: "https://wa.me/message/PLACEHOLDER",
-    displayText: "+57 304 60 500 80",
+    displayText: "+57 304 6050080",
     redirect: "wa",
     title: "WhatsApp",
   },

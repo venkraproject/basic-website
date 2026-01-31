6b5b40d71d8270b6528ac2fb68854e7d00c14620
juanr.cifuentes <juanr.cifuentes@icloud.com>
2026-01-31 12:28:24 +0100

    refactor: remove hardcoded base URL from Vite config and simplify plugin usage

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import socialRedirectsPlugin from './plugins/socialRedirects.js'
 
-const base = '/venkra--basic-website/'
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), socialRedirectsPlugin(base)],
-  base,
+  plugins: [react(), socialRedirectsPlugin()],
 })
